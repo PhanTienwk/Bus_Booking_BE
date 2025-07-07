156a79e510a9ca877fc0fa00765ffd1ee03bf70e
Phan Tien <phantienwk@gmail.com>
2025-07-07 19:50:17 +0700

    setup service

---
+++ src/main/java/com/thuctap/busbooking/service/auth/BusRouteService.java
@@ -0,0 +1,4 @@
+package com.thuctap.busbooking.service.auth;
+
+public interface BusRouteService {
+}
---
+++ src/main/java/com/thuctap/busbooking/service/impl/TicketServiceImpl.java
@@ -0,0 +1,15 @@
+package com.thuctap.busbooking.service.impl;
+
+import com.thuctap.busbooking.service.auth.TicketService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TicketServiceImpl implements TicketService {
+}
