75a7d3fe13ab801ade1f4793c11f604d88a863ee
Phan Tien <phantienwk@gmail.com>
2025-07-11 13:09:08 +0700

    auth account

--- src/main/java/com/thuctap/busbooking/controller/AccountController.java
+++ src/main/java/com/thuctap/busbooking/controller/AccountController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AccountController {
     AccountServiceImpl accountService;
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     ApiResponse<List<Account>> getAllAccount(){
         return ApiResponse.<List<Account>>builder()
